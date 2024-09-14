5ff22d06333cf70f6f8f39b3b2f8724abe5b0f7f
A. ADELL <aabd2006@gmail.com>
2024-09-14 15:17:01 +0200

    afficher.c commit

--- init_c/afficher.c
+++ init_c/afficher.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "afficher.h"
 /* compilation : gcc -c -W -Wall -std=c99 afficher.c*/
+/* g=débogage symbolique, W et Wall=messages d'avertissement(utiles lors de mise au point du programme) */
 void afficher(int t[3], size_t n)
 {
     printf("printf peut afficher ceci:\n");
